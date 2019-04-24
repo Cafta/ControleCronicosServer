a53581a3ab5e9841302b983105a8c61a4866cff9
Carlos Amaral <cafta13@yahoo.com.br>
2019-04-24 00:21:17 -0300

    Alteracoes 23/04/19

--- src/main/java/com/drcarlosamaral/controlecronicosserver/Login.java
+++ src/main/java/com/drcarlosamaral/controlecronicosserver/Login.java
@@ -25,7 +25,7 @@ import org.bson.Document;
 public class Login {
     private static String name;
     private static String URL = "mongodb://usuario:xyz123@localhost:27017";
-    public static final String bd = "ControleCronicosBD2";//"ControleCronicosBD2";//"ControleNovaAmerica"; //"ControleCronicosBD";
+    public static final String bd = "ControleCronicosBD";//"ControleCronicosBD2";//"ControleNovaAmerica"; //"ControleCronicosBD";
     public static final String VERSAO = "1.1.0 (void)";
     
     /**
